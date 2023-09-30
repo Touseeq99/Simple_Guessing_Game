664b99293b52e76a2525d038219efd6a1db8c0cc
touseeq99 <touseeqahmed2343@gmail.com>
2023-09-30 15:30:06 +0500

    All Files

---
+++ main.py
@@ -0,0 +1,23 @@
+# Guessing__game
+import random
+
+num = int(input("Guess The Number = "))
+jackpot = random.randint(0, 250000)
+counter = 0
+while True:
+    counter = counter + 1
+    if num == jackpot:
+        print("Enter Number is Correct")
+        break
+    elif num > jackpot:
+        print("Guess Lower")
+        num = int(input("Guess The Number = "))
+    
+    elif num < jackpot:
+        print("Guess Higher")
+        num = int(input("Guess The Number = "))
+    
+    else:
+        print("Out Of bound")
+
+print("You took ", counter, "too Guess")
